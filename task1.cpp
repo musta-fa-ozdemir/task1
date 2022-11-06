fed6bc83387f257a6ad0783cb24d23e6ec5b4074
musta-fa-ozdemir <117600990+musta-fa-ozdemir@users.noreply.github.com>
2022-11-06 23:46:13 +0300

    Add files via upload

---
+++ task1.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<math.h>
+using namespace std ;
+#define g 9.80665
+
+int main()
+
+{
+	float t, h;
+	
+	
+	cin>> t;
+	
+	h=(g/2)*t*t;
+	cout<<"height : "<<h<<endl;
+	
+	
+}
+
+
